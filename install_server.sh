8a3b91a3bc8ec78fc4e52a720b73a9cb30587b9e
Paul V Puey <paullinator@users.noreply.github.com>
2015-07-17 18:32:48 -0700

    Update bs.cfg to new v2.1 parameters

--- install_server.sh
+++ install_server.sh
@@ -20,21 +20,33 @@ chmod 755 bx
 
 echo "
 # Libbitcoin Server configuration file.
-[general]
-# The blockchain directory, defaults to 'blockchain'.
-blockchain_path = blockchain
+[node]
+# The number of threads in the database threadpool, defaults to 6.
+database_threads = 6
+# The number of threads in the network threadpool, defaults to 4.
+network_threads = 4
+# The number of threads in the memory threadpool, defaults to 4.
+memory_threads = 4
+# The maximum number of peer hosts in the pool, defaults to 1000.
+host_pool_capacity = 1000
+# The maximum number of orphan blocks in the pool, defaults to 50.
+block_pool_capacity = 50
+# The maximum number of transactions in the pool, defaults to 2000.
+tx_pool_capacity = 100000
 # The minimum height of the history database, defaults to 0.
 history_height = 0
-# The peer cache file path, defaults to 'hosts'.
-hosts_file = hosts
-# Enable the listening for incoming connections, defaults to true.
-listener_enabled = true
+# The height of the checkpoint hash, defaults to 0.
+checkpoint_height = 300000
+# The checkpoint hash, defaults to a null hash (no checkpoint).
+checkpoint_hash = 000000000000000082ccf8f1557c5d40b21edabb18d2d691cfbf87118bac7254
+# The port for incoming connections, set to 0 to disable, defaults to 8333 (18333 for testnet).
+# listen_port = 0
 # The maximum number of outgoing P2P network connections, defaults to 8.
-out_connections = 32
-# Enable the publisher, defaults to false.
-publisher_enabled = false
-# The maximum number of transactions in the pool, defaults to 2000.
-tx_pool_capacity = 2000
+outbound_connections = 8
+# The peer cache file path, defaults to 'peers'.
+hosts_file = peers
+# The blockchain directory, defaults to 'blockchain'.
+blockchain_path = blockchain
 
 [logging]
 # The debug log file path, defaults to 'debug.log'.
@@ -44,19 +56,21 @@ error_file = error.log
 # Write service requests to the log, impacts performance, defaults to false.
 log_requests = false
 
-[endpoints]
+[server]
 # The query service endpoint, defaults to 'tcp://*:9091'.
-service = tcp://*:9091
-# The heartbeat endpoint, defaults to 'tcp://*:9092'.
-heartbeat = tcp://*:9092
+query_endpoint = tcp://*:9091
+# The heartbeat service endpoint, defaults to 'tcp://*:9092'.
+heartbeat_endpoint = tcp://*:9092
 # The block publishing service endpoint, defaults to 'tcp://*:9093'.
-block_publish = tcp://*:9093
+block_publish_endpoint = tcp://*:9093
 # The transaction publishing service endpoint, defaults to 'tcp://*:9094'.
-tx_publish = tcp://*:9094
+tx_publish_endpoint = tcp://*:9094
+# Enable the publisher, defaults to false.
+publisher_enabled = false
 
 [identity]
 # The path to the ZPL-encoded server private certificate file.
-cert_file = ${userdir}/libbitcoin/server.cert
+#cert_file = /home/bitcoin/libbitcoin/server.cert
 # The directory for ZPL-encoded client public certificate files, allows anonymous clients if not set.
 # client_certs_path =
 # Allowed client IP address, all clients allowed if none set, multiple entries allowed.
@@ -66,7 +80,8 @@ cert_file = ${userdir}/libbitcoin/server.cert
 # peer = obelisk.airbitz.co:8333
 # peer =
 # The server name, must be unique if specified.
-# unique_name = " > bs.cfg
+# unique_name = 
+" > bs.cfg
 
 ./bx cert-new server.cert
 
